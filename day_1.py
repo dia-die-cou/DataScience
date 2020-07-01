e75c2263480ab6d9a7f85820e8f8c2e939f7a636
diego <diego.dia.cou@techtalents.club>
2020-07-01 14:03:03 +0200

    End of class 01/07

--- day_1.py
+++ day_1.py
@@ -16,60 +16,30 @@ def get_name(meteor):
 
 content = file("NASA.csv")
 # lines = content.splitlines()
+def process_meteorites(contents):
+    processors = [
+        str, int, str, str, float, str, str, float, float, str
+    ]
 
-
-meteorites = []
-processors = [
-    str, int, str, str, float, str, str, float, float, str
-]
-
-column_names = next(content)
-for row in content:
-    processed_data = []
-    for proc, data in zip(processors, row):
-        try:
-            processed_data.append(proc(data))
-        except  ValueError:
-            break
-    else:
-        # meteor = {column_names[i]: row[i] for i in range(len(column_names))}
-        meteor = {name: data for name, data in zip(column_names, processed_data)}  # Pythonic
-
-        meteorites.append(meteor)
-        
-        '''
-        if meteor["mass (g)"]:
-            meteor["mass (g)"] = float(meteor["mass (g)"])
+    column_names = next(content)
+    for row in content:
+        processed_data = []
+        for proc, data in zip(processors, row):
+            try:
+                processed_data.append(proc(data))
+            except  ValueError:
+                break
         else:
-            continue
-        '''
-'''
-# (one way not dynamic)
-next(content)
-for row in content:
-    # row = row.split(",")
-    meteor = {
-        "name": row[0],
-        "id": row[1],
-        "nametype": row[2],
-        "recclass": row[3],
-        "mass": row[4],
-        "fall": row[5],
-        "year": row[6],
-        "reclat": row[7],
-        "namereclong": row[8],
-        "GeoLocation": row[9]
-    }
-    # if meteor["mass"]:
-    #     meteor["mass"] = meteor["mass"]
+            # meteor = {column_names[i]: row[i] for i in range(len(column_names))}
+            meteor = {name: data for name, data in zip(column_names, processed_data)}  # Pythonic
+            yield meteor
 
-    meteorites.append(meteor)
-'''
+meteorites = process_meteorites(content)
 
-#'''
+# '''
 # (largest mass)
 largest = max(meteorites, key=get_mass)
-#'''
+# '''
 '''
 # (largest name lenght)
 largest = max(meteorites, key=get_name)
